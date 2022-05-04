76649d83b00477dbdfc90fc96c789ee433006292
JaredP45 <synetos@protonmail.com>
2022-05-04 15:34:35 -0500

    Update project.

--- weather_app/main.py
+++ weather_app/main.py
@@ -8,83 +8,81 @@ import requests
 from dotenv import load_dotenv
 
 
-class ConvertTemp:
-    def __init__(self, temp):
-        self.temp = temp
-
-    def C_to_F(self):
-        return (self.temp * 1.80) + 32.00
-
-    def F_to_C(self):
-        return (self.temp - 32) / 1.80
-
-
 class WeatherApp(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        # App Window
-        self.iconbitmap("jpaubel.ico")
-        self.title("JPaubel Weather App")
-        self.geometry("550x300")
+        self.title("Weather Wizard")
+        self.geometry("450x450")
         self.resizable(False, False)
 
-        self.logo = PhotoImage(file='jpaubel.png')
-        self.isClicked = False
-        self.tempLabel = 'Celsius'
+        # Title
+        self.city_head = tk.Label(self, text='Weather Wizard', font='Arial 18 bold')
+        self.city_head.grid(column=0, row=0)
 
-        # Entry
+        # City Name Entry
         self.city_name = tk.StringVar()
 
-        self.app_image = tk.Label(self, image=self.logo)
-        self.app_image.grid(column=0, row=0, sticky=NE)
+        self.city = tk.Label(self, text='Enter City Name', font='Arial 12')
+        self.city.grid(column=0, row=1, sticky=tk.W, padx=10)
 
-        self.city_head = tk.Label(self, text='Weather Wizard', font='Arial 18 bold')
-        self.city_head.grid(column=1, row=0, sticky=tk.NW, pady=5)
+        self.input_city = tk.Entry(self, textvariable=self.city_name, width=16, font='Arial 12')
+        self.input_city.grid(column=0, row=2, sticky=tk.W, padx=10)
 
-        self.city_head = tk.Label(self, text='Enter City Name', font='Arial 12')
-        self.city_head.grid(column=0, row=1, padx=25, pady=5)
+        # Zip Code Entry
+        self.zip_code = tk.StringVar()
 
-        self.inp_city = tk.Entry(self, textvariable=self.city_name, width=16, font='Arial 12')
-        self.inp_city.grid(column=0, row=2, sticky=tk.N, padx=5)
+        self.zip = tk.Label(self, text='Enter Zip Code', font='Arial 12')
+        self.zip.grid(column=0, row=1, sticky=tk.E, padx=10)
+
+        self.input_zip = tk.Entry(self, textvariable=self.zip_code, width=16, font='Arial 12')
+        self.input_zip.grid(column=0, row=2, sticky=tk.E, padx=10)
 
         # Process input, call API, and display information
-        tk.Button(self, command=self.retrieve_weather, text="Check Weather",
+        tk.Button(self, command=self.RetrieveWeather, text="Check Weather",
                   font="Arial 10", bg='lightblue', fg='black',
-                  activebackground="teal", padx=25, pady=5).grid(column=0, row=2, sticky=tk.S)
+                  activebackground="teal", padx=25, pady=5).grid(row=3, pady=10)
 
         self.weather_now = tk.Label(self, text="The Weather is: ", font='arial 12')
-        self.weather_now.grid(column=1, row=1, padx=25, pady=5)
+        self.weather_now.grid(row=4)
 
-        self.tfield = tk.Text(self, width=40, height=12)
-        self.tfield.grid(column=1, row=2, padx=25, pady=5)
-        self.tfield.config(state='disabled')
+        self.weather_response = tk.Text(self, width=40, height=12)
+        self.weather_response.grid(row=5, padx=20)
+        self.weather_response.config(state='disabled')
+
+    def ClearText(self):
+        self.input_zip.delete(0, END)
+        self.input_city.delete(0, END)
+
+    @staticmethod
+    def C_to_F(temp):
+        return (temp * 1.80) + 32.00
 
     @staticmethod
     def time_format_for_location(utc_with_tz):
         local_time = datetime.utcfromtimestamp(utc_with_tz)
         return local_time.time()
 
-    def C_to_F(self, cel):
-        if self.isClicked:
-            self.tempLabel = "Fahrenheit"
-            return (cel * 1.80) + 32.0
-
-
-    def retrieve_weather(self):
+    def RetrieveWeather(self):
         load_dotenv()
         api_key = os.environ['API_KEY']
 
-        weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + self.city_name.get() + '&appid=' + api_key
-        weather_info = requests.get(weather_url).json()
+        self.weather_response.config(state='normal')
+        self.weather_response.delete("1.0", "end")
 
-        self.tfield.config(state='normal')
-        self.tfield.delete("1.0", "end")
+        if len(self.city_name.get()) != 0:
+            search_option = self.city_name.get().capitalize()
+            weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + self.city_name.get() + '&appid=' + api_key
+            weather_info = requests.get(weather_url).json()
+        else:
+            search_option = self.zip_code.get()
+            weather_url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + self.zip_code.get() + '&appid=' + api_key
+            weather_info = requests.get(weather_url).json()
 
         if weather_info['cod'] == 200:
             kelvin = 273
-            temp = int(weather_info['main']['temp'] - kelvin)  # converting default kelvin value to Celsius
-            feels_like_temp = int(weather_info['main']['feels_like'] - kelvin)
+            temp = int(weather_info['main']['temp'] - kelvin)   # Convert from kelvin to Celsius
+            feels_like_temp = int(weather_info['main']['feels_like'] - kelvin) # Convert from kelvin to Celsius
             pressure = weather_info['main']['pressure']
             humidity = weather_info['main']['humidity']
             wind_speed = weather_info['wind']['speed'] * 3.6
@@ -97,20 +95,22 @@ class WeatherApp(tk.Tk):
             sunrise_time = self.time_format_for_location(sunrise + timezone)
             sunset_time = self.time_format_for_location(sunset + timezone)
 
-            weather = f"\n{self.city_name.get().capitalize()}" \
-                      f"\nTemperature (Celsius): {temp}°"\
-                      f"\nFeels like in (Celsius): {feels_like_temp}°" \
-                      f"\nPressure: {pressure} hPa" \
-                      f"\nHumidity: {humidity}%" \
-                      f"\nSunrise: {sunrise_time}" \
-                      f"\nSunset at: {sunset_time}" \
-                      f"\nCloud: {cloudy}%" \
-                      f"\nInfo: {description}"
+            weather = f"\n  {search_option}" \
+                      f"\n  Temperature: {self.C_to_F(temp)}°F" \
+                      f"\n  Temperature feels like: {self.C_to_F(feels_like_temp)}°F" \
+                      f"\n  Pressure: {pressure} hPa" \
+                      f"\n  Humidity: {humidity}%" \
+                      f"\n  Sunrise: {sunrise_time}" \
+                      f"\n  Sunset at: {sunset_time}" \
+                      f"\n  Cloud: {cloudy}%" \
+                      f"\n  Info: {description}"
         else:
-            weather = f"\n\t'{self.city_name.get()}' not found. \n Check spelling and try again."
+            weather = f"\n\t'{search_option}' not found. \n Check spelling and try again."
 
-        self.tfield.insert(tk.INSERT, weather)
-        self.tfield.config(state='disabled')
+        self.weather_response.insert(tk.INSERT, weather)
+        self.weather_response.config(state='disabled')
+
+        self.ClearText()
 
 
 if __name__ == '__main__':
